f613b3f11eadb98b677d71f9095aeaf15d1bd5b1
zizi andrea <ziziandrea@gmail.com>
2022-03-10 15:26:42 +0700

    1. Penggunaan Elemen Program Kotlin Literal

---
+++ src/main/kotlin/1_Literal.kt
@@ -0,0 +1,23 @@
+//contoh penggunaan Literal
+fun main(args: Array<String>) {
+    var intLiteral = 5
+    //literal tipe dasar int
+
+    var doubleLiteral = .02
+    //Literal untuk tipe dasar Double
+
+    var stringLiteral = "Zizi Andrea"
+    //Literal untuk tie dasar String
+
+    var charLiteral = '1'
+    //Literal untuk tipe dasar Char
+
+    var boolLiteral = true
+    //Literal untuk tipe dasar Boolean
+
+    println("intLiteral = " + intLiteral)
+    println("doubleLiteral = " + doubleLiteral)
+    println("stringLiteral = " + stringLiteral)
+    println("charLiteral = " + charLiteral)
+    println("boolLiteral = " + boolLiteral)
+}
