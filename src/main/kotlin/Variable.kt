5c38d5f638a2923d1e40490019c896de7c336a40
zizi andrea <ziziandrea@gmail.com>
2022-03-10 21:02:29 +0700

    1. Penggunaan Elemen Program Kotlin Variable

---
+++ src/main/kotlin/Variable.kt
@@ -0,0 +1,7 @@
+//penggunaan elemen program kotlin Variabel
+fun main(args: Array<String>) {
+    var dynamicVar = 2
+    println("Contoh dynamix variable =" + dynamicVar)
+    //secara otomatis compiler Kotlin akan mengenali bahwa variabel
+    //dynamicVar bertipe Int, hal ini identik dengan gaya bahasa pemrograman PHP dan Ruby.
+}
