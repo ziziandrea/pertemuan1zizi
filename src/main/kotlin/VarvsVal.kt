ad4871b547dc341293f6beb1f9860bfd07e1ff69
zizi andrea <ziziandrea@gmail.com>
2022-03-10 21:10:39 +0700

    1. Penggunaan Elemen Program Kotlin Var

---
+++ src/main/kotlin/VarvsVal.kt
@@ -0,0 +1,11 @@
+//penggunaan elemen program kotlin var dan val
+fun main(args: Array<String>) {
+    var varVariabel: Int
+    varVariabel = 56 //assigning first value to varVariable
+    println("Pemberian nilai pertama pada varVariable = "+varVariabel)
+    varVariabel = 78 //assigning seconf value to varVariable
+    println("Pemberian nilai kedua pada varVariable =" +varVariabel)
+
+    //var di Kotlin digunakan untuk mendeklarasikan variabel yang nilainya dapat berubah
+    //sepanjang program dieksekusi.
+}
