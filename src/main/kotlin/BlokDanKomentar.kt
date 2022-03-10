b34ac4fa2612e15f5869629029feed622e3aa867
zizi andrea <ziziandrea@gmail.com>
2022-03-10 20:58:42 +0700

    1. Penggunaan Elemen Program Kotlin Blook dan Komentar

---
+++ src/main/kotlin/BlokDanKomentar.kt
@@ -0,0 +1,13 @@
+//penggunaan elemen program kotlin Blok dan Komentar
+fun main(args: Array<String>) {
+    println("Hello, Ini Program Pertama Saya di Kotlin!")
+    //Blok digunakan untuk menulis ikatan sebuah statement yang dikelompokkan bersama menjadi satu bagian
+
+    //Contoh komentar satu baris
+    /*
+    Contoh komentar multi baris
+    */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
