de0ae4904b6e4e10b6696b673642f7061e65f3c4
zizi andrea <ziziandrea@gmail.com>
2022-03-10 20:55:15 +0700

    1. Penggunaan Elemen Program Kotlin Operator

---
+++ src/main/kotlin/Operator.kt
@@ -0,0 +1,14 @@
+//penggunaan elemen program kotlin Operator
+fun main() {
+    var hasil1 = (5 + 3) + 2 / 2
+    println("hasil1: $hasil1")
+    //kita melakukan operasi penambahan didalam tanda kurrung jadi akan dijalankan pertema setelah itu bilanganga 2/2
+
+    var hasil2 = 8 % 3
+    println("hasil2: $hasil2")
+    //kita menggunakan operator modulo untuk mencari sisa pembagian 8 dan 3
+
+    var hasil3 = 5 * 3 - 3
+    println("hasil3: $hasil3")
+    //kita mencari hasil 5*3 setelah itu baru melakukan operasi pengurangan 3
+}
