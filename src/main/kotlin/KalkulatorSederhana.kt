ae0f114ad0ff90143b3321ebd438e22cba2daa57
zizi andrea <ziziandrea@gmail.com>
2022-03-16 22:05:18 +0700

    1. Penggunaan Elemen Program Kotlin Program Kalkulator Sederhana

---
+++ src/main/kotlin/KalkulatorSederhana.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    //Simple calculator
+    println("Welcome to calculator")
+    println("Enter the first number:")
+    val a = readLine()!!.toDouble()
+    println("Enter the second number:")
+    val b = readLine()!!.toDouble()
+    val sum = a + b
+    val difference = a - b
+    val product = a * b
+    val quotient = a / b
+    println("Sum: ${sum}")
+    println("Difference: ${difference}")
+    println("Product: ${product}")
+    println("Quotient: ${quotient}")
+    println("Thank you for using calculator.")
+
+    //Program ini menerima 2 masukkan bilangan dan akan melakukan operasi tambah, kurang, kali
+    //dan bagi dari kedua bilangan yang dimasukkan.
+}
