f33410490e6eaeebac3b6a1be567148d5e4cee2b
zizi andrea <ziziandrea@gmail.com>
2022-03-16 21:51:33 +0700

    1. Penggunaan Elemen Program Kotlin Boolean

---
+++ src/main/kotlin/Boolean.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var varBool : Boolean = true
+    if(varBool)
+        println("varBool bernilai " + varBool)
+
+    //Tipe data Boolean memiliki nilai true atau false. Nilai 0 dan 1 tidak berlaku untuk menunjuk
+    //nilai pada sebuah variabel bertipe Boolean.
+}
