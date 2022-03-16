0bcc7613540e22a24b67c379eb730262b67184d7
zizi andrea <ziziandrea@gmail.com>
2022-03-16 21:56:17 +0700

    1. Penggunaan Elemen Program Kotlin String Templates

---
+++ src/main/kotlin/StringTemplates.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    val a = 7
+    val b = 8
+    val c = a + b
+    val s = "When we add $a and $b, we get $c"
+    println(s)
+    println("When we add $a and $b, we get ${a + b}")
+    val price = """
+ ${'$'}9.99
+ """
+    println(price)
+
+    //Template ekspresi String diawali dengan tanda dolar ($) diikuti oleh nama variabel yang akan
+    //digabungkan kedalam suatu String.
+}
