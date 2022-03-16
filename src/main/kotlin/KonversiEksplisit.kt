cc645cbc41ce5106354785c30fc4935d30e6f711
zizi andrea <ziziandrea@gmail.com>
2022-03-16 21:44:59 +0700

    1. Penggunaan Elemen Program Kotlin Konversi Ekplisit dalam Literal

---
+++ src/main/kotlin/KonversiEksplisit.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val valInt: Int = 1
+    val valSum: Long = 3L + valInt //konversi implisit
+    println("Konversi variabel valInt secara implisit : " + valSum)
+    val valLong: Long = valInt.toLong() // konversi eksplisit
+    println("Konversi variabel valInt secara eksplisit : " + valLong)
+
+    //Setiap Number di Kotlin mendukung konversi secara eksplisit
+}
