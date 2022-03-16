8c21b76adf42a56c756d9453dec5e248d500e2ff
zizi andrea <ziziandrea@gmail.com>
2022-03-16 21:54:05 +0700

    1. Penggunaan Elemen Program Kotlin String

---
+++ src/main/kotlin/String.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val myName: String = "Depandi Enda"
+    for(chr in myName){
+        print(chr)
+    }
+    print('\n')
+
+    //String merupakan array dari karakter dan bersifat immutable (bersifat statis). Elemen suatu
+    //String dapat diakses dengan operasi indeks array contoh s[i] atau dengan menggunakan for-loop.
+}
